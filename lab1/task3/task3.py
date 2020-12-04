30fd590900c70846282f637993e8e282f261397c
Kiril Vakulenko <wither04@gmail.com>
2020-12-04 20:27:43 +0200

    trying chi-squared

--- lab1/task3/task3.py
+++ lab1/task3/task3.py
@@ -3,43 +3,35 @@ from itertools import product
 import math
 # MAP IS COPY PASTED FROM http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
 FREQUENCY_TABLE = {
-    ' ': 13,
-    'e': 12.02,
-    't': 9.10,
-    'a': 8.12,
-    'o': 7.68,
-    'i': 7.31,
-    'n': 6.95,
-    's': 6.28,
-    'r': 6.02,
-    'h': 5.92,
-    'd': 4.32,
-    'l': 3.98,
-    # 'u': 2.88,
-    # 'c': 2.71,
-    # 'm': 2.61,
-    # 'f': 2.30,
-    # 'y': 2.11,
-    # 'w': 2.09,
-    # 'g': 2.03,
-    # 'p': 1.82,
-    # 'b': 1.49,
-    # 'v': 1.11,
-    # 'k': 0.69,
-    # 'x': 0.17,
-    # 'q': 0.11,
-    # 'j': 0.10,
-    # 'z': 0.07,
+    'e': 0.1202,
+    't': 0.091,
+    'a': 0.0812,
+    'o': 0.0768,
+    'i': 0.0731,
+    'n': 0.0695,
+    's': 0.0628,
+    'r': 0.0602,
+    'h': 0.0592,
+    'd': 0.0432,
+    'l': 0.0398,
+    'u': 0.0288,
+    'c': 0.0271,
+    'm': 0.026,
+    'f': 0.023,
+    'y': 0.0211,
+    'w': 0.0209,
+    'g': 0.0203,
+    'p': 0.0182,
+    'b': 0.0149,
+    'v': 0.0111,
+    'k': 0.0069,
+    'x': 0.0017,
+    'q': 0.0011,
+    'j': 0.001,
+    'z': 0.0007
 }
 
-MOST_COMMON_WORDS = {
-# 'of', 'to', 'in', 'it', 'is', 'be', 'as', 'at', 'so', 'we', 'he', 'by', 'or', 'on', 'do',
-# 'if', 'me', 'my', 'up', 'an', 'go', 'no', 'us', 'am',
-'the', 'and', 'for', 'are', 'but', 'not', 'you', 'all', 'any', 'can', 'had', 'her', 'was', 'one', 'our',
-'out', 'day', 'get', 'has', 'him', 'his', 'how', 'man', 'new',
-'now', 'old', 'see', 'two,' 'way', 'who', 'boy', 'did', 'its', 'let', 'put', 'say', 'she', 'too', 'use',
-'that', 'with', 'have', 'this', 'will', 'your', 'from', 'they', 'know', 'want', 'been', 'good', 'much', 'some', 'time',
-}
+
 CIPHER = (
     '1c41023f564b2a130824570e6b47046b521f3f5208201318245e0e6b40022643072e13183e51183f5a1f3e4702245d4b285a1b235619'
     '65133f2413192e571e28564b3f5b0e6b50042643072e4b023f4a4b24554b3f5b0238130425564b3c564b3c5a0727131e38564b245d07'
@@ -94,77 +86,76 @@ def print_index_of_coincidence(cipher):
 KEY_LEN = 6
 
 
-def get_characters_distribution(cipher, key_len):
-    distribution = []
-    for shift in range(key_len):
-        char_sequence = cipher[shift::key_len]
-        char_distribution = defaultdict(lambda: 0)
-        for char in char_sequence:
-            char_distribution[char] += 1
-        max_char = max(char_distribution, key=lambda char: char_distribution[char])
-        #char_distribution = {char: round(amount / len(char_sequence) * 100, 5) for char, amount in distribution.items()}
-        distribution.append(max_char)
-    return distribution
+def get_char_distribution(char_sequence):
+    char_distribution = defaultdict(lambda: 0)
+    for char in char_sequence:
+        char_distribution[char] += 1
+    return char_distribution
 
 
-def create_key(distribution, permutation):
-    return ''.join(chr(ord(char) ^ ord(swap_char)) for char, swap_char in zip(distribution, permutation))
+def split_text_to_char_sequences(text, key_len):
+    return tuple(text[shift::key_len] for shift in range(key_len))
 
 
-def calculate_deviation(actual, required):
-    return round(abs(required-actual) / required * 100)
-
-
-def get_distribution_deviation(actual, required):
-    deviation_sum = 0
-    for trigram, distribution in required.items():
-        deviation_sum += round(abs(distribution - actual.get(trigram, 0)) / distribution * 100)
-    return deviation_sum / len(required)
-
-
-deviations = defaultdict(lambda: 0)
-
-
-def is_text(text):
-    text = text.lower()
-    words_sequence = ''.join(text.split())
-    words_found = defaultdict(lambda: 0)
-    for word in MOST_COMMON_WORDS:
-        if word in words_sequence:
-            words_found[word] += 1
-    return sum(words_found.values(), 0)
+def calculate_chi_squared(char_distribution: dict, char_sequence_len: int):
+    chi_squared_sum = 0
+    for char, distribution in FREQUENCY_TABLE.items():
+        expected_char_amount = distribution * char_sequence_len
+        actual_char_amount = char_distribution[char]
+        chi_squared = (actual_char_amount - expected_char_amount) ** 2 / expected_char_amount
+        chi_squared_sum += chi_squared
+    return chi_squared_sum
 
 
 def try_decode(cipher, key_len):
-    distribution = get_characters_distribution(CIPHER, KEY_LEN)
-    print(distribution)
-    # TODO: replace with FREQUENCY_TABLE
-    l = FREQUENCY_TABLE
-    total = {}
-    tried_permutations = set()
-    for permutation in product(l, repeat=key_len):
-        if permutation in tried_permutations:
-            continue
-        tried_permutations.add(permutation)
-        key = create_key(distribution, permutation)
-        decoded = decode(cipher, key)
-        words_found = is_text(decoded)
-        if words_found:
-            total[permutation] = words_found
-    print(dict(sorted(total.items(), key=lambda a: a[1])))
-
-
-def output_sequences(cipher, key_len):
-    DISTRIBUTION = ['2', 'b', '5', '3', '0', 'b']
-    for shift in range(0, 1):
-        shifted_sequence = cipher[shift::key_len]
-        for c in FREQUENCY_TABLE:
-            print(f"====================={shift}===========================")
-            print('key', c, ord(c))
-            print(decode(shifted_sequence, create_key((DISTRIBUTION[shift],), (c,))))
-            print("=================================================")
+    from string import printable
+    char_sequences = split_text_to_char_sequences(CIPHER, KEY_LEN)
+    for sequence in char_sequences[1:2]:
+        key_to_chi_squared_statistics = {}
+        for key in range(256):
+            decoded_sequence = decode(sequence, chr(key))
+            if not all(c in printable for c in decoded_sequence) or not all(c in (' ', *FREQUENCY_TABLE) for c in decoded_sequence):
+                continue
+            sequence_len = len(decoded_sequence)
+            char_distribution = get_char_distribution(decoded_sequence)
+            chi_squared = calculate_chi_squared(char_distribution, sequence_len)
+            if len(sequence) != len(decoded_sequence):
+                continue
+            key_to_chi_squared_statistics[decoded_sequence] = chi_squared
+        s = sorted(key_to_chi_squared_statistics.items(), key=lambda item: item[1])
+        for i, statistics in s:
+            print(i)
+            print(statistics)
 
 
 if __name__ == '__main__':
     try_decode(CIPHER, KEY_LEN)
-    # output_sequences(CIPHER, KEY_LEN)
+
+    # k1 = (91, 123, 90, 122, 65, 97, 70)
+    # k2 = (91, 123, 90, 122, 93, 125, 65)
+    # k3 = (92, 124, 93, 125, 65, 97, 70)
+    # k4 = (95, 127, 94, 126, 64, 96, 91)
+    # k5 = (89, 121, 92, 124, 66, 98, 95)
+    # k6 = (91, 123, 92, 124, 90, 122, 95)
+    # res = []
+    # for a in k1:
+    #     for b in k2:
+    #         for c in k3:
+    #             for d in k4:
+    #                 for e in k5:
+    #                     for f in k6:
+    #                         key = ''.join(chr(k) for k in (a, b, c, d, e, f))
+    #                         decoded = decode(CIPHER, key).lower()
+    #                         chi = calculate_chi_squared(get_char_distribution(decoded), len(decoded))
+    #                         res.append((decoded, chi))
+    # print(sorted(res, key=lambda i: (i[1][1], i[1][0]))[:3])
+    # print(key)
+    # decoded = decode(CIPHER, key).lower()
+    # for s in range(KEY_LEN):
+    #     print(decoded[s::KEY_LEN])
+    # print(decoded)
+    # example = "%$$  %$$ $$%%%$$$ $$$% $$%$%%$%%$%$%%%%$%%%% %$  $!  %$ $$%%$$$ %$ $$ $%% $%%$$% $$$%$%$ $$ %%% $$$$$$$$$$$$%\"$% %$%$%$ %$ $$%$$$% %$  $ $$$$%%u  $%$% $ $$$$%$$&%&&$&$'%!$!!&"
+    # preformatted = preformat_sequence(example)
+    # char_distribution = get_char_distribution(preformatted)
+    # print(char_distribution)
+    # print(calculate_chi_squared(char_distribution, len(preformatted)))
