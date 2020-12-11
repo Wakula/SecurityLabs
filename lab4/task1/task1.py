df212eb24d38b33bf93dc1fe0b57d188c1a8d132
Kiril Vakulenko <wither04@gmail.com>
2020-12-12 00:25:48 +0200

    add flow to match password distribution as described in task

--- lab4/task1/task1.py
+++ lab4/task1/task1.py
@@ -21,17 +21,17 @@ percent_5_10 = tuple(percent for percent in range(1, 10+1))
 percent_50_90 = tuple(percent for percent in range(1, 90+1))
 percent_1_5 = tuple(percent for percent in range(1, 5+1))
 
-password_amount = 1000000
+password_amount = 10000000
 for _ in range(password_amount):
-    if random.randint(1, 100) in percent_5_10:
+    if random.randint(1, 100) in percent_1_5:
+        corelation['1-5'] += 1
+    elif random.randint(1, 100) in percent_5_10:
         corelation['5-10'] += 1
 
     elif random.randint(1, 100) in percent_50_90:
         corelation['50-90'] += 1
 
-    elif random.randint(1, 100) in percent_1_5:
-        corelation['1-5'] += 1
     else:
         corelation['rest'] += 1
 
-print(corelation)
+print({key: round(amount / password_amount * 100, 2) for key, amount in corelation.items()})
